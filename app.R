6a5e3a8f3bc94c7f0307224b41382a07026f38c8
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-01-13 17:03:02 +0100

    fit prelaunch ui

--- app.R
+++ app.R
@@ -3,33 +3,40 @@ library(miniUI)
 
 mtInit <- function(inputValue1, inputValue2) {
   
-  ui <- miniPage(
+  ui <- miniPage(  # http://stackoverflow.com/questions/24973549/r-shiny-key-input-binding
     tags$head(tags$script('
     $(document).on("keydown", function (e) {
       Shiny.onInputChange("keyz", e.which);
     });
     ')),
-    gadgetTitleBar("mt"),
+    gadgetTitleBar(strong('mt')),
     miniContentPanel(  # Define layout, inputs, outputs
-      h4('Provide input URLs'),
       textInput('line', NULL, '', '100%', 'Enter URLs'),
-      p('Input URLs:'),
-      verbatimTextOutput('mirror'),
+      strong('Input URLs:'),
+      htmlOutput('mirror'),
       br(),
-      h4('Set arguments')
-      
+      checkboxGroupInput('checks', 'Choose which meta info to get', 
+                         choices = list("Title"=1, "Description"=2, "Keywords"=3),
+                         selected = c(1, 2, 3)),
+      miniButtonBlock(actionButton('submit', 'Go!')),
+      htmlOutput('success')
     )
   )
 
-  server <- function(input, output, session) {
-    store <- reactiveValues(uris=character())
+  server <- function(input, output, session) {  # input$checks
+    store <- reactiveValues(uris=character(), cnvt=character(),
+                            params=list(), mrx=matrix())
     observeEvent(input$keyz, {  # observer, updates store conditionally
-      if (grepl('^https?://[^\\.]+\\.[[:lower:]]{2,10}', input$line) & input$keyz == 13)
+      if (grepl('^https?://[^\\.]+\\..{2,}', input$line) & input$keyz == 13) {
         store$uris[length(store$uris)+1] <- trimws(input$line)
-        #clear line on submit!!
+        updateTextInput(session, 'line', NULL, '')
+      }
     })
     
-    output$mirror <- renderPrint({ store$uris })  # let only store update ui #mirror
+    output$mirror <- renderUI({  # let only store update ui #mirror
+      store$cnvt <- sapply(store$uris, function(x) { paste0(x, '<br/>') })
+      HTML(paste0(store$cnvt))
+    })
     
     observeEvent(input$done, { stopApp('done') })  # @param Return value
   }
