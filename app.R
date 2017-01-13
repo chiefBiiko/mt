2f745a891064eb1cc68edcaac63ebe624a52c23d
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-01-13 13:54:37 +0100

    finally reacted

--- app.R
+++ app.R
@@ -4,25 +4,34 @@ library(miniUI)
 mtInit <- function(inputValue1, inputValue2) {
   
   ui <- miniPage(
+    tags$head(tags$script('
+    $(document).on("keydown", function (e) {
+      Shiny.onInputChange("keyz", e.which);
+    });
+    ')),
     gadgetTitleBar("mt"),
-    miniContentPanel(
-      # Define layout, inputs, outputs
+    miniContentPanel(  # Define layout, inputs, outputs
       h4('Provide input URLs'),
-      textInput('gui_txtinput', label=NULL, value='', width='100%', placeholder='Enter URLs'),
-      p('ur input: ...'),
+      textInput('line', NULL, '', '100%', 'Enter URLs'),
+      p('Input URLs:'),
+      verbatimTextOutput('mirror'),
+      br(),
       h4('Set arguments')
       
     )
   )
-  
+
   server <- function(input, output, session) {
-    # Define reactive expressions, outputs, etc.
-    
-    # When the Done button is clicked, return a value
-    observeEvent(input$done, {
-      returnValue <- 'done'
-      stopApp(returnValue)
+    store <- reactiveValues(uris=character())
+    observeEvent(input$keyz, {  # observer, updates store conditionally
+      if (grepl('^https?://[^\\.]+\\.[[:lower:]]{2,10}', input$line) & input$keyz == 13)
+        store$uris[length(store$uris)+1] <- trimws(input$line)
+        #clear line on submit!!
     })
+    
+    output$mirror <- renderPrint({ store$uris })  # let only store update ui #mirror
+    
+    observeEvent(input$done, { stopApp('done') })  # @param Return value
   }
   
   runGadget(ui, server)
