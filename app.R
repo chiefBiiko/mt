58304db23c9e633faeb0f803483a30edd36d57cc
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-01-14 10:26:23 +0100

    set in scraper

--- app.R
+++ app.R
@@ -1,33 +1,38 @@
 library(shiny)
 library(shinyjs)
 library(miniUI)
+source('C:/Users/Schwarz/Desktop/SO Banana/mt/mt.R')
 
 mtInit <- function(inputValue1, inputValue2) {
   # --------------------------------------------------------------------------
   ui <- miniPage(
     useShinyjs(),  # http://stackoverflow.com/questions/24973549/r-shiny-key-input-binding
-    tags$head(tags$script('
-        $(document).on("keydown", function (e) {
+    tags$head(
+      tags$script('
+        $(document).on("keydown", function(e) {
           Shiny.onInputChange("keyz", e.which);
         });
-    ')),
+      ')
+    ),
     gadgetTitleBar(strong('mt')),
     miniTabstripPanel(
       miniTabPanel('Parameters', icon=icon('sliders'),
                    miniContentPanel(  # 1st view
-                     textInput('line', 'Enter URLs', '', '100%', 'http:// or https://'),
-                     htmlOutput('mirror'),
-                     tags$div(id='reset-blk', actionLink('reset', 'Reset URLs'), br(), br()),
+                     textInput('line', 'Enter URLs', '', '100%', 'http://... or https://...'),
+                     hidden(htmlOutput('mirror')),
+                     hidden(tags$div(id='reset-blk', actionLink('reset', 'Reset URLs'), br(), br())),
                      checkboxGroupInput('checks', 'Choose meta info to get', 
                                         choices = list("Title"=1, "Description"=2, "Keywords"=3),
                                         selected = c(1, 2, 3)),
-                     miniButtonBlock(actionLink('submit', strong('Get info for given URLs!'))), br()
+                     miniButtonBlock(hidden(actionLink('submit', strong('Get info for given URLs!')))), br()
                    )
       ),
-      miniTabPanel('Data', icon=icon('table'),
+      miniTabPanel('Data', id='data', icon=icon('table'),
                    miniContentPanel(  # 2nd view
                      strong('Return data'),
-                     htmlOutput('success')
+                     tags$div(id='S', '$'),
+                     actionLink('view', 'View data'), br(),
+                     actionLink('save', 'Save .csv')
                    )
       )
     )
@@ -35,8 +40,8 @@ mtInit <- function(inputValue1, inputValue2) {
   # --------------------------------------------------------------------------
   server <- function(input, output, session) {  # input$checks
     store <- reactiveValues(uris=character(), cnvt=character(),
-                            params=list(), mrx=matrix())
-    lapply(list('mirror', 'reset-blk', 'submit'), hide)  # hide on launch
+                            params=character(), mrx=matrix())
+    #lapply(list('mirror', 'reset-blk', 'submit', ''), hide)  # hide on launch
     # event listener 4 js keydown, updating store$uris
     observeEvent(input$keyz, {
       if (input$keyz == 13 & grepl('^https?://[^\\.]+\\..{2,}', input$line)) {
@@ -58,7 +63,16 @@ mtInit <- function(inputValue1, inputValue2) {
       HTML(paste0(store$cnvt))
     })
     # TODO: get btn and returning
-    
+    observeEvent(input$submit, {
+      store$params <- c('1' %in% input$checks, '2' %in% input$checks, '3' %in% input$checks)
+      store$mrx <- mt$read(store$uris, store$params[1], store$params[2], store$params[3])
+    })
+    # switch to 2nd view?
+    #
+    # view btn
+    observeEvent(input$view, { View(store$mrx) })
+    # save btn
+    observeEvent(input$save, { write.csv(store$mrx, 'mt.csv') }) 
   }
   # --------------------------------------------------------------------------
   runGadget(ui, server)
