940d7196905a13544e42e40f1c73d61a3cf2d3c9
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-01-20 14:54:05 +0100

    serving returns in a list

--- mt.R
+++ mt.R
@@ -1,18 +1,14 @@
 # mt
-# TODO: scrape more meta tags such as og:...
-#       collapse sequential regex procedures into a single operation! 
-# String prefixes:
-#   d_ dirty
-#   t_ tidy
 # ----------------------------------------------------------------------------
 mt <- list()
 # ----------------------------------------------------------------------------
 # Reads meta information from webpages
-#   @param {char} URI Vector of URIs to scan
-#   @param {bool} titl Get title text
-#   @param {bool} desc Get meta description text
-#   @param {bool} keyw Get meta keywords text
-#   @param {bool} schema Get schema.org JSON
+# @param {char} URI Vector of URIs to scan
+# @param {bool} titl Get title text
+# @param {bool} desc Get meta description text
+# @param {bool} keyw Get meta keywords text
+# @param {bool} schema Get schema.org JSON
+# @return {list} $meta of class matrix, and if schema==T $schema of class list 
 # ----------------------------------------------------------------------------
 mt$read <- function(URI=NULL, titl=T, desc=T, keyw=T, schema=T) {
   if (missing(URI)) stop('no input URI!')
@@ -26,31 +22,25 @@ mt$read <- function(URI=NULL, titl=T, desc=T, keyw=T, schema=T) {
     rtn <- c(x)
     CON <- url(x)
     on.exit(close(CON))
-    d_src <- paste(try(readLines(CON, warn=F)), collapse='')
+    d_src <- paste0(try(readLines(CON, warn=F)), collapse='')
     if (titl) {
-      if(grepl('<title>.*</title>', d_src)) {
-        t_titl <- gsub('.*<title>|</title>.*', '', d_src)
-        rtn <- c(rtn, t_titl)
+      if (grepl('<title>.*</title>', d_src)) {
+        rtn <- c(rtn, gsub('.*<title>|</title>.*', '', d_src))
       } else { rtn <- c(rtn, NA) }
     }
     if (desc) {
       if (grepl('meta name=\"description\"', d_src)) {
-        d_desc <- gsub('^.*(<meta name=\"description\"[^>]+>).*$', '\\1', d_src)
-        t_desc <- gsub('^.*content=\"([^"]+)".*$', '\\1', d_desc)
-        rtn <- c(rtn, t_desc)
+        rtn <- c(rtn, gsub('^.*<meta[^"]+name=\"description\"[^"]+content=\"([^"]+)".+$', '\\1', d_src))
       } else { rtn <- c(rtn, NA) }
     }
     if (keyw) {
       if (grepl('meta name=\"keywords\"', d_src)) {
-        d_keyw <- gsub('^.*(<meta name=\"keywords\"[^>]+>).*$', '\\1', d_src)
-        t_keyw <- gsub('^.*content=\"([^"]+)".*$', '\\1', d_keyw)
-        rtn <- c(rtn, t_keyw)
+        rtn <- c(rtn, gsub('^.*<meta[^"]+name=\"keywords\"[^"]+content=\"([^"]+)".+$', '\\1', d_src))
       } else { rtn <- c(rtn, NA) }
     }
     if (schema) {  # binding schema json to a separate list outside sapply's scope
       if (grepl('<script type=\"application/ld\\+json\"', d_src)) {
-        t_schm <- gsub('^.*<script type=\"application/ld\\+json\">([^<]+)</script>.*$', '\\1', d_src)
-        data$schema[[x]] <<- fromJSON(t_schm)
+        data$schema[[x]] <<- fromJSON(gsub('^.*<script type=\"application/ld\\+json\">([^<]+)</script>.*$', '\\1', d_src))
       } else { data$schema[[x]] <<- NA }
     }
     return(rtn)
