d90990fb06eb0c5e6e72332590432521ab75ba33
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-11-24 12:11:56 +0100

    back 2 da basics

--- drop/filepaths-group/index.js
+++ drop/filepaths-group/index.js
@@ -1,6 +1,5 @@
 const fs = require('fs')
 const path = require('path')
-const ops = require('pojo-ops')
 const countEntries = require('./count-top-entries/index')
 const wishlist = require('./wishlist/index')
 
@@ -15,63 +14,63 @@ function gotAllNestedFiles (dir, map, cb) { // cb tells the truth
       wishlist(dir, { full: true }, function (err, wishlist) {
         if (err) return cb(err)
         wishlist.dirs.forEach(function (d) {
-          console.log('wishlist full dir',d)
           gotAllNestedFiles(d, map, cb)
-          // gotAllNestedFiles(path.join(dir, d), map, cb)
         })
       })
     }
   })
 }
 
-function group (filepaths, callback) {
-  const trap = {
-    entireDirectories: [],
-    singleFiles: [],
-    _files: [],
-    _map: {},
-    _temp: []
-  }
+function maybeVerbose (opts, trap) {
+  if (opts.verbose) return trap
+  else return { entireDirs: trap.entireDirs, singleFiles: trap.singleFiles }
+}
+
+function group (filepaths, opts, callback) {
+  if (typeof opts === 'function') return group(filepaths, {}, opts)
   if (!callback) throw new Error('gimme a callback, gonna callback(err, data)')
+  var trap = { entireDirs: [], singleFiles: [], _f: [], _m: {}, _t: [] }
   // split filepaths into file objects
-  trap._files = filepaths.map(function (filepath) {
-    return {
-      name: filepath.replace(/^.+(\/|\\)(.+)$/, '$2'),
-      path: filepath,
-      dir: filepath.replace(/^(.+)(\/|\\).*$/, '$1')
-    }
+  trap._f = filepaths.map(function (filepath) {
+    return { path: filepath, dir: filepath.replace(/^(.+)(\/|\\).*$/, '$1') }
   })
   // if single file input always early return as singleFile
-  if (trap._files.length === 1) {
-    trap.singleFiles.push(trap._files[0].path)
+  if (trap._f.length === 1) {
+    trap.singleFiles.push(trap._f[0].path)
     return callback(null, trap)
   }
   // map files to dirs
-  trap._map = trap._files.reduce(function (acc, cur) {
-    acc.hasOwnProperty(cur.dir)
-      ? acc[cur.dir].push(cur.path) : acc[cur.dir] = [ cur.path ]
+  trap._m = trap._f.reduce(function (acc, cur) {
+    if (acc.hasOwnProperty(cur.dir)) acc[cur.dir].push(cur.path)
+    else acc[cur.dir] = [ cur.path ]
     return acc
   }, {})
   // push keys of props that represent an entire dir to trap.entir... via temp
-  var pending = ops.size(trap._map)
-  ops.keys(trap._map).forEach(function (dir) {
-    gotAllNestedFiles(dir, trap._map, function (err, truth) {
+  var dirs = Object.keys(trap._m)
+  var pending = dirs.length
+  dirs.forEach(function (dir) {
+    gotAllNestedFiles(dir, trap._m, function (err, truth) {
       if (err) return callback(err, null)
-      if (truth) trap._temp.push(dir)
+      if (truth) trap._t.push(dir)
       if (!--pending) finishUp()
     })
   })
   // finish
   function finishUp () {
-    // push filepaths that are not covered by trap._temp to trap.singleFiles
-    trap.singleFiles.push(...trap._files.filter(function (file) {
-      return !trap._temp.some(dir => dir === file.dir)
-    }).map(file => file.path))
-    // collapse nested dirs in temp to trap.entireDirectories
-    trap.entireDirectories.push(...trap._temp.filter(function (dir, i, arr) {
-      return !arr.filter(d => d !== dir).some(other => dir.startsWith(other))
-    }))
-    callback(null, trap)
+    // push filepaths that are not covered by trap._t to trap.singleFiles
+    Array.prototype.push.apply(trap.singleFiles,
+      trap._f.filter(function (file) {
+        return !trap._t.some(function (dir) { return dir === file.dir })
+      }).map(function (file) { return file.path })
+    )
+    // collapse nested dirs in temp to trap.entireDirs
+    Array.prototype.push.apply(trap.entireDirs,
+      trap._t.filter(function (dir, i, arr) {
+        var others = arr.filter(function (d) { return d !== dir })
+        return !others.some(function (other) { return dir.startsWith(other) })
+      })
+    )
+    callback(null, maybeVerbose(opts, trap))
   }
 }
 
